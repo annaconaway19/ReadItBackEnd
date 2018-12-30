41c5dcc14cba5952319cf4e69d4dac7313a02e41
github username <github email address>
2018-12-30 13:29:44 -0500

    more login

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -3,9 +3,17 @@ class Api::V1::AuthController < ApplicationController
   def create
     @reader = Reader.find_by(username: params[:username])
     if @reader && @reader.authenticate(params[:password])
-      render json: {message: "correct username and password"}, status: :accepted
+      render json: {
+        message: "correct username and password",
+        reader_info: @reader,
+        error: false,
+        }, status: :accepted
+
     else
-      render json: {message: "incorrect!"}, status: :unauthorized
+      render json: {
+        message: "incorrect!",
+        error: true,
+        }, status: :unauthorized
     end
   end
 
